f24342f3843405acb14473d6bd51aaf191f8a0ba
krish <guptashanu341@gmail.com>
2024-05-20 19:50:05 +0530

    6

--- lect2.js
+++ lect2.js
@@ -21,7 +21,7 @@
     // console.log(square);
     // filter
     const name=["england,finland,india"];
-    const land = name.filter((country) =>country.en\d)
+    const land = name.filter((country) =>country.end)
     
 
 
