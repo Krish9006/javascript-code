5a21e8c233ff27d0956454cef1197a7e6cc006ea
krish <guptashanu341@gmail.com>
2024-05-22 21:22:06 +0530

    11

--- lect2.js
+++ lect2.js
@@ -1,27 +1,41 @@
-// const callback = (n)=>{
-//     return n**2;
-// };
-// function cube(){
-//     return callback(n)*2;
-// }
-// console.log(cube(callback,3);
-// function gret (){
-//     console.log("hell");
+// // const callback = (n)=>{
+// //     return n**2;
+// // };
+// // function cube(){
+// //     return callback(n)*2;
+// // }
+// // console.log(cube(callback,3);
+// // function gret (){
+// //     console.log("hell");
 
-// }
-// setInterval(gret,1000);
-// setInterval(()=>{
-//     console.log("hell");
-//     },1000);
-    // setTimeout(() => {
-    //     console.log("hell");
-    //     },5000);
-    // const num =[1,2,3,4]
-    // const square =num.map((num) =>num*num);
-    // console.log(square);
-    // filter
-    const name=["england,finland,india"];
-    const land = name.filter((country) =>country.end)
-    
+// // }
+// // setInterval(gret,1000);
+// // setInterval(()=>{
+// //     console.log("hell");
+// //     },1000);
+//     // setTimeout(() => {
+//     //     console.log("hell");
+//     //     },5000);
+//     // const num =[1,2,3,4]
+//     // const square =num.map((num) =>num*num);
+//     // console.log(square);
+//     // filter
+//     // const country=["england,finland,india"];
+//     // const land = name.filter((country) =>country
+//     // endwith("land");
+//     // console.log(land);
+//     //reduce
+//     // const numb=[1,2,3,4,5];
+//     // const vari= numb.reduce((acc.curr) => acc+curr,10);
+//     // console.log(vari);
+//     const num1 =[1,2,34,4];
+//     let sum =0;
+//     num1.forEach((num)=> {
+//         sum += num;
+
+//     });console.log(sum);
+// sort
+const arr= ["awadh","dev","krish"];
+console.log(arr.sort());
 
 
