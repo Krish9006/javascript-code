c621155a0d2a9c9df2c6e8b0116507508a4bcaf4
krish <guptashanu341@gmail.com>
2024-05-29 10:06:17 +0530

    44

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var a = 10;
-let b= 20;
-sum =a+b;
-console.log(sum);
+// const var1 = document.getElementsByTagName(h1);
+// console.log(var1);
+const var2=document.getElementById("first-id");
+console.log(var2);
+var2.outerText = "nameste";
