977ac9eee1c3df52a38905813c07aa13c99e5274
krish <guptashanu341@gmail.com>
2024-06-06 11:09:42 +0530

    78

---
+++ index.html project_1/index.html project_2/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const taskInput = document.getElementById("taskbutton","addTaskButton","TaskList");
+    const addTaskButton = document.getElementById("addTaskButton");
+    const taskList = document.getElementById("TaskList");
+         addTaskButton.addEventListener("click",);
+         
+
+    
+})
