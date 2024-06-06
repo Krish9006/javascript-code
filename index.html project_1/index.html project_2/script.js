17532a917dc15f578e3fe0cabbd62fb583a0aef3
krish <guptashanu341@gmail.com>
2024-06-06 11:30:36 +0530

    80

--- index.html project_1/index.html project_2/script.js
+++ index.html project_1/index.html project_2/script.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const taskInput = document.getElementById("taskbutton","addTaskButton","TaskList");
+    document.addEventListener("DOMContentLoaded",()=>{
+    const taskInput = document.getElementById("taskButton");
     const addTaskButton = document.getElementById("addTaskButton");
-    const taskList = document.getElementById("TaskList");
-         addTaskButton.addEventListener("click",);
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click",addTask);
+
+    function addTask(){
+     const taskText = taskInput.value.trim();
+
+
+     if(taskText === "") {
+     alert("Enter your task");
+     return;
+}
+      const taskitem=document.createElement("li");
+      taskitem.textContent = taskText;
+      const deletebutton =document.createElement("button");
+      deletebutton.textContent= "Delete";
          
 
-    
-})
+} 
+});
